1abdfd1c2ffbd200def3146afef0f35cdfede503
Sneha Sharma <snehasharma9205@gmail.com>
2025-03-15 20:43:22 +0530

    server-connection-done

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
- 
+const express = require("express")
+
+const app = express()
+
+app.listen(3000, ()=>{
+    console.log("server started at 3000")
+})
+
+app.get('/health', (req,res)=>{
+    res.send("Healthy")
+})
